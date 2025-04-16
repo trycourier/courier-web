e92f576104cdca581b72796317a577aa062507e9
Mike Miller <mike@mikemiller.design>
2025-04-16 17:18:23 -0600

    Jest tooling WIP

--- @trycourier/courier-js/jest.config.js
+++ @trycourier/courier-js/jest.config.js
@@ -1,10 +1,22 @@
-export default {
-  preset: 'ts-jest',
+export default /** @type {import('ts-jest').JestConfigWithTsJest} */ ({
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+
+  extensionsToTreatAsEsm: ['.ts'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      { useESM: true, tsconfig: './tsconfig.json' }
+    ]
   },
+
+  transformIgnorePatterns: ['/node_modules/(?!@trycourier/)'],
+  moduleNameMapper: { '^(\\.{1,2}/.*)\\.js$': '$1' },
+
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/__tests__/**/*.ts?(x)',
+    '**/?(*.)+(spec|test).ts?(x)'
+  ],
   setupFiles: ['<rootDir>/src/jest.setup.ts']
-};
+});
--- @trycourier/courier-js/src/__tests__/brand-client.test.ts
+++ @trycourier/courier-js/src/__tests__/brand-client.test.ts
@@ -1,17 +1,10 @@
-import { CourierClient } from '../client/courier-client';
 import { getClient } from './utils';
 
 describe('BrandClient', () => {
   const courierClient = getClient();
 
   it('should fetch brand settings successfully', async () => {
-
-    const client = new CourierClient({
-      userId: '1234567890',
-      jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6InVzZXJfaWQ6bWlrZSB3cml0ZTp1c2VyLXRva2VucyBpbmJveDpyZWFkOm1lc3NhZ2VzIGluYm94OndyaXRlOmV2ZW50cyByZWFkOnByZWZlcmVuY2VzIHdyaXRlOnByZWZlcmVuY2VzIHJlYWQ6bGlzdHMgd3JpdGU6bGlzdHMgd3JpdGU6dHJhY2siLCJ0ZW5hbnRfc2NvcGUiOiJwdWJsaXNoZWQvcHJvZHVjdGlvbiIsInRlbmFudF9pZCI6ImQ5NDllNmMwLTg1ZjgtNDI4NC05NWNjLWNiZjM2YzRjMjlhYiIsImlhdCI6MTc0NDg0MDc2NiwiZXhwIjoxODMxMjQwNzY2LCJqdGkiOiJjZjY4ZTFjZi03ZGNiLTQ0MDMtYTA1Mi1mNzBmNGYzNTU2Y2UifQ.dmxOb-wrsvle0q0epT3mX3qFcjRMzjDJZ2V6meKLF0k'
-    });
-
-    const brand = await client.brands.getBrand({ brandId: 'YF16AVZZ574DF6MTY73E40F6CHH0' });
+    const brand = await courierClient.brands.getBrand({ brandId: 'YF16AVZZ574DF6MTY73E40F6CHH0' });
     expect(brand.settings?.inapp?.disableCourierFooter).toBeDefined();
   });
 
