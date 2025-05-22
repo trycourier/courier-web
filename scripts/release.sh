3b9ac87078d47d17be9c0b6c652b223d24895557
Mike Miller <mike@mikemiller.design>
2025-05-22 17:53:27 -0500

    MERGE

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,1 +1,10 @@
-echo "Releasing package: $1"
+#!/bin/sh
+
+# Run git checks
+sh "$(dirname "$0")/check_git.sh" || exit 1
+
+# Run git checks
+sh "$(dirname "$0")/incremement_version.sh" "$1" || exit 1
+
+# Run push release
+sh "$(dirname "$0")/release_package.sh" "$1" || exit 1
