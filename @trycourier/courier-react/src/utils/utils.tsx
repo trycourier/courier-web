de0e9b904ce5a278e57033115efa2c88f7eee674
Mike Miller <mike@mikemiller.design>
2025-06-03 13:03:34 -0600

    React 17 attempt

--- @trycourier/courier-react/src/utils/utils.tsx
+++ @trycourier/courier-react/src/utils/utils.tsx
@@ -1,43 +1,11 @@
 import { ReactNode } from "react";
-import * as ReactDOM from "react-dom";
-
-let client: any | null = null;
-const { flushSync } = ReactDOM as any;
+import Renderer from "../renderer/renderer";
 
 /**
  * Converts React node to HTMLElement with synchronous updates
  */
 export function reactNodeToHTMLElement(node: ReactNode): HTMLElement {
-  const container = document.createElement("div");
-  flushSync(() => renderCompat(node, container));
+  const container = document.createElement('div');
+  Renderer.render(node, container);
   return container;
-}
-
-export async function renderCompat(
-  node: ReactNode,
-  container: HTMLElement
-): Promise<void> {
-  let createRoot = (ReactDOM as any).createRoot;
-
-  if (!createRoot) {
-    try {
-      if (client === null) {
-        client = await import("react-dom/client");
-      }
-      createRoot = client.createRoot;
-    } catch {
-      /* React 18+ only */
-    }
-  }
-
-  if (typeof createRoot === "function") {
-    createRoot(container).render(node);
-  } else if (typeof (ReactDOM as any).render === "function") {
-    (ReactDOM as any).render(node, container);
-  } else {
-    throw new Error(
-      "Neither ReactDOM.createRoot nor ReactDOM.render is available - " +
-      "check your react-dom version."
-    );
-  }
 }
