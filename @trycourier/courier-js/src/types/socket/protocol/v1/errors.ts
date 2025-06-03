9cd4f571499df1051cfdbc082c7aa9f51aadb4ec
Dana Silver <dana@courier.com>
2025-06-03 14:14:55 -0700

    Remove reason regex.

--- @trycourier/courier-js/src/types/socket/protocol/v1/errors.ts
+++ @trycourier/courier-js/src/types/socket/protocol/v1/errors.ts
@@ -1,14 +1,4 @@
 /**
- * Regular expression to parse Courier-specific close event reasons.
- *
- * The WebSocket CloseEvent.reason property an opaque string up to interpretation
- * by the application layer.
- *
- * @see https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent/reason
- */
-export const COURIER_CLOSE_REASON = /(?<reason>.*) \((?<payload>{.*})\)/;
-
-/**
  * Connection close code for non-error conditions.
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent/code
