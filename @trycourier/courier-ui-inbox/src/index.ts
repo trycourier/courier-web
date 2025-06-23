a2fc3163ddb024a6fd6fa78597324429d56cc26a
Mike Miller <mike@mikemiller.design>
2025-06-23 15:11:17 -0600

    Refresh button fixes

--- @trycourier/courier-ui-inbox/src/index.ts
+++ @trycourier/courier-ui-inbox/src/index.ts
@@ -11,3 +11,27 @@ export * from './types/inbox-data-set';
 export * from './datastore/datastore';
 export * from './datastore/datastore-listener';
 export * from './datastore/datatore-events';
+
+// Re-export Courier from courier-js for direct import
+export { Courier } from '@trycourier/courier-js';
+
+// Re-export types from courier-js
+export type {
+  CourierProps,
+  CourierClientOptions,
+  CourierBrand,
+  CourierApiUrls,
+  CourierUserPreferences,
+  CourierUserPreferencesStatus,
+  CourierUserPreferencesChannel,
+  CourierUserPreferencesPaging,
+  CourierUserPreferencesTopic,
+  CourierUserPreferencesTopicResponse,
+  CourierDevice,
+  CourierToken,
+  CourierGetInboxMessageResponse,
+  CourierGetInboxMessagesResponse,
+  InboxMessage,
+  InboxAction,
+  InboxMessageEventEnvelope,
+} from '@trycourier/courier-js';
