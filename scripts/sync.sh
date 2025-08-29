7576b8fcfc194c7222bfe6433bfb40b78d8cff75
Dana Silver <dana@courier.com>
2025-08-29 11:07:19 -0700

    Use build-packages in sync script.

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Get the directory where the script is located
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -6,22 +6,11 @@ ROOT_DIR="$(dirname "$SCRIPT_DIR")"
 
 source "$SCRIPT_DIR/nvm-use.sh"
 
-# Function to clean up dependencies
-cleanup_dependencies() {
-    cd "$ROOT_DIR"
-    rm -rf **/node_modules
-    yarn install
-}
-
+cd "$ROOT_DIR"
 nvm_use
 
-# Re-install dependencies
-cleanup_dependencies
+# Make sure deps are up to date
+yarn install
 
-# Build all packages
-yarn workspace @trycourier/courier-js run build
-yarn workspace @trycourier/courier-ui-core run build
-yarn workspace @trycourier/courier-ui-inbox run build
-yarn workspace @trycourier/courier-react-components run build
-yarn workspace @trycourier/courier-react run build
-yarn workspace @trycourier/courier-react-17 run build
+# Build all @trycourier/* packages
+yarn build-packages
