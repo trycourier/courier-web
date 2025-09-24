ad66f4efd4237162f1544356c464eaa7559543ee
Dana Silver <dana@courier.com>
2025-09-23 17:15:52 -0700

    Add examples with toasts.

---
+++ examples/react-latest/src/Toast.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useRef } from 'react'
+import { CourierToast, useCourier } from '@trycourier/courier-react'
+import type { CourierToast as CourierToastElement } from '@trycourier/courier-ui-inbox';
+
+export default function App() {
+
+  const courier = useCourier();
+  const el = useRef<CourierToastElement>(null);
+
+  useEffect(() => {
+    courier.shared.signIn({
+      userId: import.meta.env.VITE_USER_ID,
+      jwt: import.meta.env.VITE_JWT,
+    });
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      el.current?.addInboxMessage({
+        messageId: "1",
+        title: "We've been working on something...",
+        body: "👀"
+      });
+    }, 2000);
+
+    setTimeout(() => {
+      el.current?.addInboxMessage({
+        messageId: "1",
+        title: "A bit of the same...",
+        body: "But a whole lot that's new"
+      });
+    }, 5000);
+
+    setTimeout(() => {
+      el.current?.addInboxMessage({
+        messageId: "1",
+        title: "Re-introducing Toasts",
+        body: "Dismissible in-app messages, fully customizable and synced with Courier Inbox"
+      });
+    }, 8000);
+
+  }, []);
+
+  return <CourierToast autoDismiss={true} ref={el} autoDismissTimeoutMs={10000} />;
+
+
+}
