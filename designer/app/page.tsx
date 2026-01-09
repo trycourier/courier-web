105e5acfca389774262c34f103290465f55619ba
Mike Miller <mike@mikemiller.design>
2026-01-09 11:19:16 -0700

    Redirect with params

--- designer/app/page.tsx
+++ designer/app/page.tsx
@@ -1,5 +1,7 @@
 import { redirect } from 'next/navigation';
 
 export default function RootPage() {
+  // Middleware handles the redirect with query params
+  // This is a fallback in case middleware doesn't run
   redirect('/inbox-demo');
 }
